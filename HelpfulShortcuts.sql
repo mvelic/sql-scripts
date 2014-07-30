1a5cd217c1f04ed8ef286551e33bc079e3e7e3e7
Matt Velic <mattvelic@gmail.com>
2014-07-30 19:16:06 -0400

    Some reorganization and renaming.

---
+++ HelpfulShortcuts.sql
@@ -0,0 +1,21 @@
+/*
+
+# Helpful Keyboard Shortcuts
+
+- Obviously, these only work in SQL Server Management Studio
+- For a full list of shortcuts, visit http://msdn.microsoft.com/en-us/library/ms174205.aspx
+
+*/
+
+/*
+
+F5 => Execute query
+
+Ctrl + K + C => Comment Out SQL (Can be multiline)
+Ctrl + K + U => Uncomment SQL (Can be multiline)
+
+Ctrl + Shift + M => Fill in template
+
+Alt + F1 => Runs sp_help system stored procedure
+
+*/
